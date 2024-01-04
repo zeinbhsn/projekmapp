bf56ff5531b1e1c134b351184fa33a4d1673fda2
ibrahim <ibrahimms11@yahoo.com>
2024-01-04 20:50:13 +0700

    activity bayar

---
+++ app/src/main/java/com/example/projek/bayar.kt
@@ -0,0 +1,34 @@
+package com.example.projek
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class SimpleCalculatingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bayar)
+
+        val calculateButton = findViewById<Button>(R.id.calculateButton)
+        calculateButton.setOnClickListener { calculate() }
+    }
+
+    private fun calculate() {
+        val input = findViewById<EditText>(R.id.inputEditText)
+        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+
+        val inputText = input.text.toString()
+        if (inputText.isEmpty()) {
+            resultTextView.text = "Please enter a number."
+            return
+        }
+
+        val inputNumber = inputText.toInt()
+        val result = inputNumber * 35000
+        resultTextView.text = "Result: $result ribu"
+    }
+}
